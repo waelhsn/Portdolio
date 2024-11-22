dcdddde4bb9cbb113365a0b14fb2f64729d98a10
Wael <wael.hasan@preem.se>
2024-11-22 09:17:10 +0100

    ssdf

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ const prependDollarSign = (selector) => {
 
 // Apply to both <h3> and <p> elements
 prependDollarSign('h3');
-prependDollarSign('li');
-    
+
+const hashSgin = (selector) => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+        element.textContent = `# ${element.textContent.trim()}`;
+    });
+};
+
+// Apply to both <h3> and <p> elements
+hashSgin('h4');
+
 (function() {
     emailjs.init('FLtAw4eSXjYUo51wN'); // Replace with your EmailJS user ID
 })();
@@ -18,13 +27,20 @@ prependDollarSign('li');
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    const form = this;
     const formStatus = document.getElementById('form-status');
     formStatus.textContent = 'Sending...';
 
-    emailjs.sendForm('service_id_test', 'template_0ixtwzh', this)
+    const formData = {
+        name: form.name.value,
+        email: form.email.value,
+        message: form.message.value
+    };
+
+    emailjs.send('service_id_test', 'template_0ixtwzh', formData)
         .then(() => {
-            formStatus.textContent = 'Message sent successfully!';
-        },  (error) => {
+            formStatus.textContent = `Message sent successfully to ${formData.name} (${formData.email})!`;
+        }, (error) => {
             formStatus.textContent = 'Failed to send message. Please try again later.';
             console.error('Failed to send email:', error);
         });
